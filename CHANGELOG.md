fb3363df286c4f2d6678688df93609a51f568081
antkey1 <antillarionov@gmail.com>
2022-05-04 13:08:02 +0300

    fix labels instead on values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 Changelog
 ===
 
+# 0.0.3
+* Fix prometheus labels (instead of values)
+
 # 0.0.2
 * Fix prometheus labels (replace dashes with underscores)
 
--- yandex_compute_sd/apps/web/factory.py
+++ yandex_compute_sd/apps/web/factory.py
@@ -6,7 +6,7 @@ from .routes import api
 def create_application() -> FastAPI:
     app = FastAPI(
         title='YC Compute Prometheus SD',
-        version='0.0.2',
+        version='0.0.3',
     )
     app.include_router(router=api.router, prefix='/api')
     return app
